0957380882a5968649ecdd876df8c87786a3bb67
Dharaneeswar reddy Rami reddy <130501038+DharaneeswarReddy-Ramireddy@users.noreply.github.com>
2024-10-21 11:28:52 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,47 +1,9 @@
-variable "aws_region" {
-  description = "The AWS region to deploy in"
-  type        = string
-  default     = "us-west-2"
-}
+# variables.tf
 
-variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
-  type        = string
-  default     = "18.0.0.0/16"
-}
+variable "region" {
 
-variable "public_subnet_cidr" {
-  description = "The CIDR block for the public subnet"
-  type        = string
-  default     = "18.0.1.0/24"
-}
+  description = "Region for my manifest. The AMI is tied to this region"
 
-variable "private_subnet_cidr" {
-  description = "The CIDR block for the private subnet"
-  type        = string
-  default     = "18.0.2.0/24"
-}
+  default     = "us-east-2"
 
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
-}
-
-variable "desired_capacity" {
-  description = "Desired capacity of Auto Scaling Group"
-  type        = number
-  default     = 2
-}
-
-variable "max_size" {
-  description = "Maximum size of Auto Scaling Group"
-  type        = number
-  default     = 2
-}
-
-variable "min_size" {
-  description = "Minimum size of Auto Scaling Group"
-  type        = number
-  default     = 1
 }
